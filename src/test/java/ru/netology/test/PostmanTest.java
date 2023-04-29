77030577737c8f56bef0f3294427baaec689f957
Andrey Kucherenko <andykucherenko@gmail.com>
2023-04-29 19:49:37 +0300

    Initial commit

---
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanTest {
+    @Test
+    void postmanMessageTest() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Message!") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Message!"));
+    }
+}
